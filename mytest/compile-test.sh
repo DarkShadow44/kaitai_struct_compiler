24f441b3ae6963ff06f421aea882b2114c856433
Fabian Maurer <dark.shadow4@web.de>
2021-10-02 08:27:43 +0200

    compile tests with errors

--- mytest/compile-test.sh
+++ mytest/compile-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -I ../../runtime/c *.c ../../runtime/c/*.c -o test.app
+gcc -I ../../runtime/c *.c ../../runtime/c/*.c -o test.app -Wall -Werror
 #g++ -I ../../runtime/cpp_stl *.cpp ../../runtime/cpp_stl/*.cpp -o test.app
