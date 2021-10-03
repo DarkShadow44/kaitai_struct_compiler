f4b95dfb728af304f12b295fd152fa2ea959a270
Fabian Maurer <dark.shadow4@web.de>
2021-10-03 13:26:02 +0200

    Use c98 std

--- mytest/compile-test.sh
+++ mytest/compile-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -I ../../runtime/c *.c ../../runtime/c/*.c -o test.app -Wall -Werror
+gcc -I ../../runtime/c *.c ../../runtime/c/*.c -o test.app -Wall -Werror -Wno-unused-function -std=c89
 #g++ -I ../../runtime/cpp_stl *.cpp ../../runtime/cpp_stl/*.cpp -o test.app
