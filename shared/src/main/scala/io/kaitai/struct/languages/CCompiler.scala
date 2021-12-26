e7d4d9983dd8fb87354f90ad0c84d541681d1e31
Fabian Maurer <dark.shadow4@web.de>
2021-12-26 05:21:26 +0100

    Fix seeking to use correct io

--- shared/src/main/scala/io/kaitai/struct/languages/CCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/languages/CCompiler.scala
@@ -520,7 +520,7 @@ class CCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
   }
 
   override def seek(io: String, pos: Ast.expr): Unit =
-    outMethodBody.puts(s"CHECKV(ks_stream_seek(stream, ${expression(pos)}));")
+    outMethodBody.puts(s"CHECKV(ks_stream_seek(${makeIO(io)}, ${expression(pos)}));")
 
   override def popPos(io: String): Unit = {
     // outMethodBody.puts(s"$io.Seek(_pos);")
